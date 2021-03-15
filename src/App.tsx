de7693215155465f5759caf47e325bbe76d33233
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-03-15 11:04:49 -0300

    main component app up-to-date

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <div>Hello (:</div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
 
