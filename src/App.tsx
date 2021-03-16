e4cdb91fac44e04540f345e789f0ae35912cc11a
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-03-16 15:24:14 -0300

    wrapped with context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { TransactionProvider } from "./hooks/TransactionsContext";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isTransactionModalOpen, setIsTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsTransactionModalOpen(false);
+  }
+  
   return (
-    <>
-      <Header />
+    <TransactionProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isTransactionModalOpen}
+        onCloseRequest={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
-    </>
+    </TransactionProvider>
   );
 }
 
