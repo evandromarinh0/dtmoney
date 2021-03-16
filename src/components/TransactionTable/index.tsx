26c96990a3383c7ebb0a18ef0f8a28e068afce4d
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-03-16 11:38:17 -0300

    connection to api done

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+
 import { 
   Container
 } from "./styles";
 
+interface Transection {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionTable() {
+  const [transactions, setTransections] = useState<Transection[]>([]);
+
+  useEffect(() => {
+    api.get('/transactions').then(response => {
+      setTransections(response.data.transactions);
+    })
+  }, []);
+
   return(
     <Container>
       <table>
@@ -16,19 +36,19 @@ export function TransactionTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className='deposit'>R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+            <td>{transaction.title}</td>
+            <td className={transaction.type}>
+              {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+              }).format(transaction.amount)}
+            </td>
+            <td>{transaction.category}</td>
+            <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
           </tr>
-
-          <tr>
-            <td>Compra - Sea of Thieves</td>
-            <td className='withdraw'>-R$90</td>
-            <td>Entretenimento</td>
-            <td>21/02/2021</td>
-          </tr>
+          ))}
         </tbody>
       </table>
     </Container>
